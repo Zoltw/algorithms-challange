3c2316ddf1b96be963d636bd122d23f57b9421e7
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-05-02 23:45:47 +0200

    added polindrome tasks

---
+++ Task2/palindromeLinked.js
@@ -0,0 +1,16 @@
+var isPalindrome = function(head) {
+    let string1 = string2 = "";
+    let node = head;
+    
+      while(node != null){
+          string1 = `${string1}${node.val}`;
+          string2 = `${node.val}${string2}`;
+          node = node.next;
+      }
+      return string1 === string2;
+  }
+
+function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+}
---
+++ Task2/polindrome.js
@@ -0,0 +1,16 @@
+var isPalindrome = function(head) {
+    if(head.length % 2 === 0) {
+       const first = head.slice(0, head.length/2);
+       const second = head.slice(head.length/2);
+       const rev = second.reverse();
+       for(let i = 0; i < first.length; i++) {
+            if(first[i] !== rev[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+console.log(isPalindrome([1,2,2,1]));
